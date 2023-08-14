26a1f0b8cfd0ce66af3c11770b047edbf2d995a6
Anthony Medina <anthony.john.medina@gmail.com>
2023-08-14 16:07:47 -0400

    section 1 completed

---
+++ src/index.js
@@ -0,0 +1,2 @@
+var awesome = "It's going to be awesomeeeee!";
+document.getElementsByTagName('p')[0].innerText = awesome;
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const awesome = "It's going to be awesomeeeee!"
+
+document.getElementsByTagName('p')[0].innerText = awesome
+
+// tsc <file.name/> --watch is used to "watch" the file as we work on it, so that on save, the recompile happens right away. 
+
+// Additionally, typescript compiles the ts to EJS5 which is the most accessible version of JS to all softwares. 
