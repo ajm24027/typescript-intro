338c029fd5b1d52a7c5b65ed30d291c43c37b590
Anthony Medina <anthony.john.medina@gmail.com>
2023-08-16 14:33:14 -0400

    Updated Objects.ts

--- src/object.ts
+++ src/object.ts
@@ -8,3 +8,12 @@ function printCoords(coords: { lat: string; lng: string}) {
 function getDistanceBetween (coords1: { lat: string; lng: string}, coords2: { lat: string; lng: string}) {
   //return distance between coords1 and coords 2
 }
+
+const user = {name: 'Codiku', lastName: 'TheDev'}
+
+function displayUser(user: {
+  firstName: string;
+  lastName?: string;
+}) {
+  console.log('Last Name', user.lastName?.toLocaleLowerCase());
+}
